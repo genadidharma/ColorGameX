8a8923961a9f996e5d7ab808ea37e96414231cca
Genadi Dharma <genadislawa@gmail.com>
2021-09-21 14:23:59 +0800

    B1 Task 5 - Countdown Timer

--- app/src/main/java/org/aplas/colorgamex/MyActivity.java
+++ app/src/main/java/org/aplas/colorgamex/MyActivity.java
@@ -3,6 +3,7 @@ package org.aplas.colorgamex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,6 +12,8 @@ import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -29,31 +32,36 @@ public class MyActivity extends AppCompatActivity {
     ProgressBar progress;
     Switch isMinus;
 
+    final String FORMAT = "%d:%d";
+    CountDownTimer countDown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
 
-        timer = (TextView)findViewById(R.id.timerText);
-        clrText = (TextView)findViewById(R.id.clrText);
-        scoreText = (TextView)findViewById(R.id.scoreText);
+        timer = (TextView) findViewById(R.id.timerText);
+        clrText = (TextView) findViewById(R.id.clrText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
         passwd = (EditText) findViewById(R.id.appCode);
         submit = (Button) findViewById(R.id.submitBtn);
-        start = (Button)findViewById(R.id.startBtn);
-        accessbox = (ViewGroup)findViewById(R.id.accessBox);
-        colorbox = (ViewGroup)findViewById(R.id.colorBox);
-        buttonbox1 = (ViewGroup)findViewById(R.id.buttonBox1);
-        buttonbox2 = (ViewGroup)findViewById(R.id.buttonBox2);
-        scorebox = (ViewGroup)findViewById(R.id.scoreBox);
-        progressbox = (ViewGroup)findViewById(R.id.progressBox);
+        start = (Button) findViewById(R.id.startBtn);
+        accessbox = (ViewGroup) findViewById(R.id.accessBox);
+        colorbox = (ViewGroup) findViewById(R.id.colorBox);
+        buttonbox1 = (ViewGroup) findViewById(R.id.buttonBox1);
+        buttonbox2 = (ViewGroup) findViewById(R.id.buttonBox2);
+        scorebox = (ViewGroup) findViewById(R.id.scoreBox);
+        progressbox = (ViewGroup) findViewById(R.id.progressBox);
         progress = (ProgressBar) findViewById(R.id.progressScore);
         isMinus = (Switch) findViewById(R.id.isMinus);
+
+        initTimer();
     }
 
     public void openGame(View view) {
         String keyword = getString(R.string.keyword);
 
-        if(keyword.equals(passwd.getText().toString())){
+        if (keyword.equals(passwd.getText().toString())) {
             passwd.setVisibility(View.INVISIBLE);
             submit.setVisibility(View.INVISIBLE);
             accessbox.setVisibility(View.VISIBLE);
@@ -63,7 +71,7 @@ public class MyActivity extends AppCompatActivity {
             scorebox.setVisibility(View.VISIBLE);
             progressbox.setVisibility(View.VISIBLE);
             Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             passwd.setVisibility(View.VISIBLE);
             submit.setVisibility(View.VISIBLE);
             accessbox.setVisibility(View.VISIBLE);
@@ -80,5 +88,19 @@ public class MyActivity extends AppCompatActivity {
     }
 
     public void startGame(View view) {
+    }
+
+    private void initTimer() {
+        countDown = new CountDownTimer((long) getResources().getInteger(R.integer.maxtimer) * 1000, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer.setText("" + String.format(FORMAT, TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)), TimeUnit.MILLISECONDS.toMillis(millisUntilFinished) - TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished))));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
     }
 }
